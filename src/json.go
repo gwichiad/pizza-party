89a2e42b8be2382b3d8716348e65073d7e6236dc
gwichiad <gwichiad@gwichiad.co.ua>
2026-08-04 10:16:19 +0200

    fix json template struct

--- src/json.go
+++ src/json.go
@@ -3,20 +3,20 @@ package main
 import "encoding/json"
 
 type SatelliteResponse struct {
-	sensor_name string `json:"sensor_name"`
-	temperature float32 `json:"temperature"`
-	pressure float32 `json:"pressure"`
-	position []struct {
-		city string `json:"city"`
-		height float32 `json:"height"`
+	Sensor_name string `json:"sensor_name"`
+	Temperature float32 `json:"temperature"`
+	Pressure float32 `json:"pressure"`
+	Position []struct {
+		City string `json:"city"`
+		Height float32 `json:"height"`
 	}
-	time string `json:"time"`
-	info string `json:"info"`
-	specs []struct {
-		name string `json:"name"`
-		model string `json:"model"`
-		launchDate string `json:"launchDate`
-		sensors string `json:"sensors"`
-		nation string `json:nation`
+	Time string `json:"time"`
+	Info string `json:"info"`
+	Specs []struct {
+		Name string `json:"name"`
+		Model string `json:"model"`
+		LaunchDate string `json:"launchDate"`
+		Sensors string `json:"sensors"`
+		Nation string `json:"nation"`
 	}
 }
